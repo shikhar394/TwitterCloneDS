faea7e42048c7676f3a96a37bb4ec0bc4945240d
rajmanglani <rm4806@nyu.edu>
2018-04-06 17:47:08 -0400

    Added login functionality. Please change the storage from list to external file (add logic for that)

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import(
   "net/http"
   "fmt"
   "container/list"
+  "strings"
 )
 
 type User struct{
@@ -19,6 +20,7 @@ var Users = list.New()
 func main() {
   http.HandleFunc("/", loginHandler)
   http.HandleFunc("/signup", signupHandler)
+  http.HandleFunc("/home", homeHandler)
   http.ListenAndServe(":8000", nil)
 }
 
@@ -31,6 +33,17 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 
   user_email := r.FormValue("email")
   user_password := r.FormValue("password")
+
+  //ckeck if user is in the gloabl Users List
+  for u:=Users.Front(); u!=nil; u=u.Next(){
+    if strings.EqualFold(u.Value.(*User).Email, user_email) && u.Value.(*User).Password == user_password{
+      http.Redirect(w,r,"/home",302)
+      return
+    }
+  }
+  //If no user found
+  tmpl.Execute(w, struct{ Authfail bool }{true})
+
   fmt.Print(user_email + " : " + user_password + "\n")
 
 }
@@ -42,15 +55,22 @@ func signupHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 
-  newUser := User{
-    Email: r.FormValue("new_email"),
-    FirstName: r.FormValue("new_fname"),
-    LastName: r.FormValue("new_lname"),
-    Password: r.FormValue("new_password"),
-  }
+  newUser := new(User)
+  newUser.Email = r.FormValue("new_email")
+  newUser.FirstName = r.FormValue("new_fname")
+  newUser.LastName = r.FormValue("new_lname")
+  newUser.Password = r.FormValue("new_password")
 
   Users.PushBack(newUser)
-  fmt.Print(Users.Front())
+  fmt.Print(Users.Front().Value.(*User).FirstName)
   tmpl.Execute(w, struct{ Success bool }{true})
 
+}
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+  tmpl := template.Must(template.ParseFiles("home.html"))
+  if r.Method != http.MethodPost {
+			tmpl.Execute(w, nil)
+			return
+		}
 }
